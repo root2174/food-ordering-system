bae603ddc3d541176f9f6ae7d5df8351e55dad1c
Lucas Magalhaes <lm031719@gmail.com>
2023-10-14 10:45:46 -0300

    Implementing Order Application Service - DTO classes

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/OrderItem.java
@@ -0,0 +1,23 @@
+package com.food.ordering.system.order.service.domain.dto.create;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public record OrderItem(
+        @NotNull
+        UUID productId,
+        @NotNull
+        Integer quantity,
+        @NotNull
+        BigDecimal price,
+        @NotNull
+        BigDecimal subtotal) {
+}
